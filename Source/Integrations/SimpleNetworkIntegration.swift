d0a374a2c6758bf7378656a4940df1ed9957a940
Alex Hill <alexhill.c@gmail.com>
2017-08-05 13:04:20 -0700

    - Fixes #4

--- Source/Integrations/SimpleNetworkIntegration.swift
+++ Source/Integrations/SimpleNetworkIntegration.swift
@@ -6,20 +6,21 @@
 //  Copyright © 2017 Alex Hill. All rights reserved.
 //
 
-open class SimpleNetworkIntegration: NSObject, NetworkIntegrationProtocol, URLSessionDataDelegate, URLSessionTaskDelegate {
+open class SimpleNetworkIntegration: NSObject, NetworkIntegrationProtocol, SimpleNetworkIntegrationURLSessionWrapperDelegate {
     
-    fileprivate var urlSession: URLSession!
+    fileprivate var urlSessionWrapper = SimpleNetworkIntegrationURLSessionWrapper()
     public weak var delegate: NetworkIntegrationDelegate?
-    
-    fileprivate var receivedData = [Int: Data]()
-    fileprivate var receivedContentLength = [Int: Int64]()
     
     fileprivate var dataTasks = NSMapTable<PhotoProtocol, URLSessionDataTask>(keyOptions: .strongMemory, valueOptions: .strongMemory)
     fileprivate var photos = [Int: PhotoProtocol]()
     
-    override public init() {
+    public override init() {
         super.init()
-        self.urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        self.urlSessionWrapper.delegate = self
+    }
+    
+    deinit {
+        self.urlSessionWrapper.invalidate()
     }
     
     public func loadPhoto(_ photo: PhotoProtocol) {
@@ -31,7 +32,7 @@ open class SimpleNetworkIntegration: NSObject, NetworkIntegrationProtocol, URLSe
             return
         }
         
-        let dataTask = self.urlSession.dataTask(with: url)
+        let dataTask = self.urlSessionWrapper.dataDask(with: url)
         self.dataTasks.setObject(dataTask, forKey: photo)
         self.photos[dataTask.taskIdentifier] = photo
         dataTask.resume()
@@ -53,56 +54,45 @@ open class SimpleNetworkIntegration: NSObject, NetworkIntegrationProtocol, URLSe
         }
         
         self.dataTasks.removeAllObjects()
-        self.receivedData.removeAll()
-        self.receivedContentLength.removeAll()
         self.photos.removeAll()
     }
     
-    // MARK: - URLSessionDataDelegate
-    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        self.receivedData[dataTask.taskIdentifier]?.append(data)
+    // MARK: - SimpleNetworkIntegrationURLSessionWrapperDelegate
+    fileprivate func urlSessionWrapper(_ urlSessionWrapper: SimpleNetworkIntegrationURLSessionWrapper,
+                                       dataTask: URLSessionDataTask, 
+                                       didUpdateProgress progress: CGFloat) {
         
-        guard let receivedData = self.receivedData[dataTask.taskIdentifier],
-            let expectedContentLength = self.receivedContentLength[dataTask.taskIdentifier],
-            let photo = self.photos[dataTask.taskIdentifier] else {
+        guard let photo = self.photos[dataTask.taskIdentifier] else {
             return
         }
         
-        self.delegate?.networkIntegration?(self, didUpdateLoadingProgress: CGFloat(receivedData.count) / CGFloat(expectedContentLength),
+        self.delegate?.networkIntegration?(self,
+                                           didUpdateLoadingProgress: progress,
                                            for: photo)
     }
     
-    public func urlSession(_ session: URLSession, 
-                           dataTask: URLSessionDataTask,
-                           didReceive response: URLResponse, 
-                           completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+    fileprivate func urlSessionWrapper(_ urlSessionWrapper: SimpleNetworkIntegrationURLSessionWrapper,
+                                       task: URLSessionTask,
+                                       didCompleteWithError error: Error?,
+                                       object: Any?) {
         
-        self.receivedContentLength[dataTask.taskIdentifier] = response.expectedContentLength
-        self.receivedData[dataTask.taskIdentifier] = Data()
-        completionHandler(.allow)
-    }
-    
-    // MARK: - URLSessionTaskDelegate
-    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         guard let photo = self.photos[task.taskIdentifier] else {
             return
         }
         
         weak var weakSelf = self
-        func removeData() {
-            weakSelf?.photos.removeValue(forKey: task.taskIdentifier)
-            weakSelf?.receivedData.removeValue(forKey: task.taskIdentifier)
-            weakSelf?.receivedContentLength.removeValue(forKey: task.taskIdentifier)
-            weakSelf?.dataTasks.removeObject(forKey: photo)
+        func removeDataTask() {
+            self.photos.removeValue(forKey: task.taskIdentifier)
+            self.dataTasks.removeObject(forKey: photo)
         }
         
         if let error = error {
             self.delegate?.networkIntegration(self, loadDidFailWith: error, for: photo)
-            removeData()
+            removeDataTask()
             return
         }
         
-        guard let data = self.receivedData[task.taskIdentifier] else {
+        guard let data = object as? Data else {
             return
         }
         
@@ -112,8 +102,93 @@ open class SimpleNetworkIntegration: NSObject, NetworkIntegrationProtocol, URLSe
             photo.image = UIImage(data: data)
         }
         
-        removeData()
+        removeDataTask()
         self.delegate?.networkIntegration(self, loadDidFinishWith: photo)
     }
 
+}
+
+// This wrapper abstracts the `URLSession` away from `SimpleNetworkIntegration` in order to prevent a retain cycle
+// between the `URLSession` and its delegate
+fileprivate class SimpleNetworkIntegrationURLSessionWrapper: NSObject, URLSessionDataDelegate, URLSessionTaskDelegate {
+    
+    weak var delegate: SimpleNetworkIntegrationURLSessionWrapperDelegate?
+    
+    fileprivate var urlSession: URLSession!
+    fileprivate var receivedData = [Int: Data]()
+    fileprivate var receivedContentLength = [Int: Int64]()
+    
+    override init() {
+        super.init()
+        self.urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+    }
+    
+    func dataDask(with url: URL) -> URLSessionDataTask {
+        return self.urlSession.dataTask(with: url)
+    }
+    
+    func invalidate() {
+        self.urlSession.invalidateAndCancel()
+    }
+    
+    // MARK: - URLSessionDataDelegate
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        self.receivedData[dataTask.taskIdentifier]?.append(data)
+        
+        guard let receivedData = self.receivedData[dataTask.taskIdentifier],
+            let expectedContentLength = self.receivedContentLength[dataTask.taskIdentifier] else {
+            return
+        }
+        
+        self.delegate?.urlSessionWrapper(self,
+                                         dataTask: dataTask,
+                                         didUpdateProgress: CGFloat(receivedData.count) / CGFloat(expectedContentLength))
+    }
+
+    func urlSession(_ session: URLSession,
+                    dataTask: URLSessionDataTask,
+                    didReceive response: URLResponse, 
+                    completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        
+        self.receivedContentLength[dataTask.taskIdentifier] = response.expectedContentLength
+        self.receivedData[dataTask.taskIdentifier] = Data()
+        completionHandler(.allow)
+    }
+    
+    // MARK: - URLSessionTaskDelegate
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        weak var weakSelf = self
+        func removeData() {
+            weakSelf?.receivedData.removeValue(forKey: task.taskIdentifier)
+            weakSelf?.receivedContentLength.removeValue(forKey: task.taskIdentifier)
+        }
+        
+        if let error = error {
+            self.delegate?.urlSessionWrapper(self, task: task, didCompleteWithError: error, object: nil)
+            removeData()
+            return
+        }
+        
+        guard let data = self.receivedData[task.taskIdentifier] else {
+            self.delegate?.urlSessionWrapper(self, task: task, didCompleteWithError: nil, object: nil)
+            removeData()
+            return
+        }
+        
+        self.delegate?.urlSessionWrapper(self, task: task, didCompleteWithError: nil, object: data)
+        removeData()
+    }
+    
+}
+
+fileprivate protocol SimpleNetworkIntegrationURLSessionWrapperDelegate: NSObjectProtocol, AnyObject {
+    
+    func urlSessionWrapper(_ urlSessionWrapper: SimpleNetworkIntegrationURLSessionWrapper,
+                           dataTask: URLSessionDataTask,
+                           didUpdateProgress progress: CGFloat)
+    func urlSessionWrapper(_ urlSessionWrapper: SimpleNetworkIntegrationURLSessionWrapper,
+                           task: URLSessionTask,
+                           didCompleteWithError error: Error?,
+                           object: Any?)
+    
 }
