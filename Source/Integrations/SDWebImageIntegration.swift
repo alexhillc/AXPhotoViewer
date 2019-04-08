7fba100f117500135a26e208bfb7da0690984e2d
Alex Hill <alexhill.c@gmail.com>
2019-04-07 20:35:01 -0700

    Stylistic updates

--- Source/Integrations/SDWebImageIntegration.swift
+++ Source/Integrations/SDWebImageIntegration.swift
@@ -18,56 +18,36 @@ class SDWebImageIntegration: NSObject, AXNetworkIntegrationProtocol {
     func loadPhoto(_ photo: AXPhotoProtocol) {
         if photo.imageData != nil || photo.image != nil {
             AXDispatchUtils.executeInBackground { [weak self] in
-                guard let `self` = self else {
-                    return
-                }
-                
+                guard let `self` = self else { return }
                 self.delegate?.networkIntegration(self, loadDidFinishWith: photo)
             }
             return
         }
         
-        guard let url = photo.url else {
-            return
-        }
+        guard let url = photo.url else { return }
                 
         let progress: SDWebImageDownloaderProgressBlock = { [weak self] (receivedSize, expectedSize, targetURL) in
-            guard let `self` = self else {
-                return
-            }
-            
             AXDispatchUtils.executeInBackground { [weak self] in
-                guard let `self` = self else {
-                    return
-                }
-             
+                guard let `self` = self else { return }
                 self.delegate?.networkIntegration?(self, didUpdateLoadingProgress: CGFloat(receivedSize) / CGFloat(expectedSize), for: photo)
             }
         }
         
         let completion: SDInternalCompletionBlock = { [weak self] (image, data, error, cacheType, finished, imageURL) in
-            guard let `self` = self else {
-                return
-            }
+            guard let `self` = self else { return }
             
             self.downloadOperations.removeObject(forKey: photo)
             
             if let data = data, data.containsGIF() {
                 photo.imageData = data
                 AXDispatchUtils.executeInBackground { [weak self] in
-                    guard let `self` = self else {
-                        return
-                    }
-                    
+                    guard let `self` = self else { return }
                     self.delegate?.networkIntegration(self, loadDidFinishWith: photo)
                 }
             } else if let image = image {
                 photo.image = image
                 AXDispatchUtils.executeInBackground { [weak self] in
-                    guard let `self` = self else {
-                        return
-                    }
-                    
+                    guard let `self` = self else { return }
                     self.delegate?.networkIntegration(self, loadDidFinishWith: photo)
                 }
             } else {
@@ -77,27 +57,18 @@ class SDWebImageIntegration: NSObject, AXNetworkIntegrationProtocol {
                     userInfo: nil
                 )
                 AXDispatchUtils.executeInBackground { [weak self] in
-                    guard let `self` = self else {
-                        return
-                    }
-                 
+                    guard let `self` = self else { return }
                     self.delegate?.networkIntegration(self, loadDidFailWith: error, for: photo)
                 }
             }
         }
         
-        guard let operation = SDWebImageManager.shared().loadImage(with: url, options: [], progress: progress, completed: completion) else {
-            return
-        }
-        
+        guard let operation = SDWebImageManager.shared.loadImage(with: url, options: [], progress: progress, completed: completion) else { return }
         self.downloadOperations.setObject(operation, forKey: photo)
     }
     
     func cancelLoad(for photo: AXPhotoProtocol) {
-        guard let downloadOperation = self.downloadOperations.object(forKey: photo) else {
-            return
-        }
-        
+        guard let downloadOperation = self.downloadOperations.object(forKey: photo) else { return }
         downloadOperation.cancel()
     }
     
