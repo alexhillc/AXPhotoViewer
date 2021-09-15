bf5f850f2e6cd3a6f6df21e5ab1a8a8e8a561897
Xiao Xiao <xx2004xiamen@gmail.com>
2021-09-15 17:55:56 +0900

    Workaround for Xcode 13 issue 74120874

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AXPhotoViewer",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
